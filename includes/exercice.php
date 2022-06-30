e3d8a2a1ee3e75943d993afdb340fd074ab0aac4
Quentin Cliquot <quentincliquot@gmail.com>
2022-06-30 14:56:06 +0200

    jour 2.4

--- includes/exercice.php
+++ includes/exercice.php
@@ -15,4 +15,35 @@ function pair($data) // fonction qui retourne les elements d'un tableau par nomb
     echo '</ul>';
 }
 
-echo (pair($tab));
+// echo (pair($tab));
+
+//=======================================================
+
+$user = [
+    "firstname" => 'Mike',
+    "lastname" => 'Olganh',
+    "pseudo" => '',
+    "age" => '34',
+    "food" => '',
+    "sport" => '',
+
+];
+debug($user);
+
+function vide($data)
+{
+    echo '<ul>';
+
+    foreach ($data as $key => $value) {
+        $rien = '"Aucune valeur"';
+
+        if ($value == NULL) {
+            echo "<li>$key : $rien</li>";
+        } else {
+            echo "<li>$key : $value</li>";
+        }
+    }
+    echo '</ul>';
+}
+
+echo vide($user);
