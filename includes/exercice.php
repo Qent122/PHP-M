d268ac2d5f3f258923326c7bbf5928e82d6069a1
Quentin Cliquot <quentincliquot@gmail.com>
2022-06-30 11:50:45 +0200

    jour 2.3.1

--- includes/exercice.php
+++ includes/exercice.php
@@ -1,7 +1,7 @@
 <?php
 $tab = [27, 15, 34, 379, 248, 5643, 81, 211, 999, 142, 300, 572];
 
-function pair($data)
+function pair($data) // fonction qui retourne les elements d'un tableau par nombre pair ou impaire sous forme de liste html.
 {
     echo '<ul>';
 
--- includes/header.php
+++ includes/header.php
@@ -6,9 +6,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP</title>
+    <style>
+        header {
+            text-align: center;
+        }
+    </style>
 </head>
 
 <body>
+
     <header>
         <h1>Les tableaux en PHP</h1>
         <?php echo date('d' . '/' . 'm' . '/' . 'Y'); ?>
